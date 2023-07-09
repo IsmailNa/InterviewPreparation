ca83f2416ef6f91d293ea7f108a8ffd572832780
sarthakkumarpanda <sarthak.matrix@gmail.com>
2023-07-09 12:26:50 -0400

    day2

---
+++ src/main/java/com/interviewprepSoftwareTesting/day2/ImportantInterview_Questions.java
@@ -0,0 +1,27 @@
+package com.interviewprepSoftwareTesting.day2;
+
+public class ImportantInterview_Questions {
+//2 or 3 people are taking your interview together
+	
+	//1 person will ask you technical questions
+	//1 person might ask you related to how good a team member you are
+	//basic knowledge of testing
+	
+	//do you know something about us
+	//scenario based questions:- Automation, basic understanding
+	
+	//basic understanding scenario based questions:-
+	//1. what are the features you need to check/test before buying a mobile/laptop?
+	//2. quality vs performance
+	//3. test cases for pen, pencil, elevator, cup of coffee
+	//4. create an application for OTP
+	
+	//Direct questions:-
+	
+	//what do you think a tester is required to know before testing the software application?
+	//how will you test desktop based applications vs web-based vs app?
+	//various anamolies in a software ?
+	//what are types of defects ?? - arithmetic, logical, syntax, multithreading, interface, performance
+	
+
+}
